4448359baa02ebec8b1315b0a38908731d6b1ded
Jonathan Chak <jonathanchak@hotmail.com>
2023-10-22 11:51:26 -0400

    Added button component to create new Flashcards

--- flashcards-conhacks-2023/src/App.jsx
+++ flashcards-conhacks-2023/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import Flashcard from "./Flashcard";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
+  const [flashcards, setFlashcards] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [term, setTerm] = useState("");
+  const [definition, setDefinition] = useState("");
+
+  const handleNewFlashcard = () => {
+    if (term && definition) {
+      setFlashcards([...flashcards, { term, definition }]);
+      setTerm("");
+      setDefinition("");
+      setShowModal(false);
+    }
+  };
+
   return (
     <>
       <div>
@@ -17,10 +32,36 @@ function App() {
       <h1>Flashcards - ConHacks 2023</h1>
 
       <div>
-        <Flashcard
-          term="Group 7 - Byte Brawlers"
-          definition="Dario Simpson, Jonathan Chak, Nav, Siyang He"
-        />
+        {flashcards.map((flashcard, index) => (
+          <Flashcard
+            key={index}
+            term={flashcard.term}
+            definition={flashcard.definition}
+          />
+        ))}
+
+        <button onClick={() => setShowModal(true)}>Add Flashcard</button>
+
+        {showModal && (
+          <div className="modal open">
+            <div className="modal-header">New Flashcard</div>
+            <input
+              className="modal-input"
+              placeholder="Term"
+              value={term}
+              onChange={(e) => setTerm(e.target.value)}
+            />
+            <input
+              className="modal-input"
+              placeholder="Definition"
+              value={definition}
+              onChange={(e) => setDefinition(e.target.value)}
+            />
+            <button className="modal-button" onClick={handleNewFlashcard}>
+              Add
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
