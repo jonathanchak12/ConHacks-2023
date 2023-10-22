3a657711c66b7e39ca9382d17c5962dbf4036e93
Jonathan Chak <jonathanchak@hotmail.com>
2023-10-22 16:10:44 -0400

    Updated CSS styling

--- flashcards-conhacks-2023/src/pages/Courses.jsx
+++ flashcards-conhacks-2023/src/pages/Courses.jsx
@@ -1,5 +1,31 @@
+import "./Courses.css";
+
+function Button({ label, onClick }) {
+  return (
+    <button className="course-buttons" onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
 function Courses() {
-  return <h1>Courses</h1>;
+  return (
+    <div className="courses-page">
+      <h1>Courses</h1>
+      <Button
+        label="New"
+        onClick={() => {
+          /* Create New Course */
+        }}
+      />
+      <Button
+        label="Remove"
+        onClick={() => {
+          /* Select course to remove */
+        }}
+      />
+    </div>
+  );
 }
 
 export default Courses;
